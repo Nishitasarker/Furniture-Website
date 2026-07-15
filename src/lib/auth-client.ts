9f2e13900e2b60fcc04a36c4f9160ae0559e25e1
Nishitasarker <shuvrodevsaikat71@gmail.com>
2026-07-15 17:20:33 +0600

    update auth.ts and auth-client.ts file

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
-});
+  baseURL: typeof window !== "undefined" 
+    ? window.location.origin 
+    : "https://e-commerce-app-seven-rho-12.vercel.app"});
 
 // এখান থেকেই সব এক্সপোর্ট করুন
 export const { signIn, signUp, useSession } = authClient;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,7 +7,7 @@ if (!process.env.MONGO_DB_URI) throw new Error("MONGO_DB_URI is missing");
 const client = new MongoClient(process.env.MONGO_DB_URI);
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL,
+ baseURL: "https://e-commerce-app-seven-rho-12.vercel.app",
   database: mongodbAdapter(client.db(process.env.AUTH_DB_NAME || "furniture_db")),
   emailAndPassword: { enabled: true },
   secret: process.env.BETTER_AUTH_SECRET!,
